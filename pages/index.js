313e7262ba6adce4dc5ad413d05899cb5f64cadd
ryoheisato <thesugar87@gmail.com>
2020-01-25 12:30:06 +0900

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+export default () => (
+    <Layout header="aaaa" title="Top page">
+        <div>
+            Hello shohan!
+        </div>
+    </Layout>
+);
