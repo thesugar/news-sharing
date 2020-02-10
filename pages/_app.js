5b69717b81cc9a31a94fc3590611821177aaef33
Ryohei Sato <thesugar87@gmail.com>
2020-02-10 16:32:22 +0900

    implemented redux-persist so that users are logged in after reloading

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,16 @@ import { Provider } from 'react-redux';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
 class _App extends App {
+
+  constructor(props){
+    super(props);
+    this.persistor = persistStore(props.reduxStore);
+  }
+
   componentDidMount() {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -24,7 +32,12 @@ class _App extends App {
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
           <Provider store={reduxStore}>
-            <Component {...pageProps} />
+            <PersistGate
+            loading={<Component {...pageProps} />}
+            persistor={this.persistor}
+            >
+              <Component {...pageProps} />
+            </PersistGate>
           </Provider>
         </ThemeProvider>
       </React.Fragment>
