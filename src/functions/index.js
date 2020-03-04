0f85bab4ef0ba7550e90adc92040817aa6e512d4
ryoheisato <thesugar87@gmail.com>
2020-03-04 12:04:21 +0900

    fix typo

--- src/functions/index.js
+++ src/functions/index.js
@@ -4,17 +4,11 @@ const next = require('next')
 
 var dev = process.env.NODE_ENV !== 'production'
 
-/*
 var app = next({
   dev,
   conf: { distDir: `${path.relative(process.cwd(), __dirname)}/next` },
 })
-*/
 
-const app = next({
-  dev: false,
-  conf: { distDir: `${path.relative(process.cwd(), __dirname)}/functions/.next` }
-});
 var handle = app.getRequestHandler()
 
 exports.next = functions.https.onRequest((req, res) => {
