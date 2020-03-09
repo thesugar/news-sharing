3e45289dd4d9ec7bfcd7f9c2ae71b3d1e0147ea9
ryoheisato <thesugar87@gmail.com>
2020-03-09 21:42:03 +0900

    fixed how to show error page

--- src/app/pages/_error.js
+++ src/app/pages/_error.js
@@ -1,16 +1,4 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import MuiLink from '@material-ui/core/Link';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import NewsList from '../components/NewsList';
-import Bar from '../components/Bar';
-import SharedNewsList from '../components/SharedNewsList';
-import { connect } from 'react-redux';
-import Head from 'next/head';
-
+import React from 'react'
 
 export default class Error extends React.Component {
   static getInitialProps({ res, err }) {
@@ -20,23 +8,9 @@ export default class Error extends React.Component {
 
   render() {
     return (
-      <Container disableGutters={true} maxWidth='lg'>
-      <Head><title>Shohan News App</title></Head>
-      <Bar />
-      <Box my={4}>
-        <Typography variant="inherit" color="textPrimary" gutterBottom>
-        <div>
-          <p>申し訳ございません。何らかのエラーが発生しました。</p>
-          {`エラーコード：${this.props.statusCode}`}
-          <MuiLink href='https://github.com/thesugar/news-sharing/issues'>
-              <a>バグ報告はこちらから</a>
-          </MuiLink>
-          </div>
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+      <p>
+        {`${this.props.statusCode}エラーが起こりました。`}
+      </p>
     )
   }
 }
