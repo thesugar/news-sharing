212ca44e16d04cf266eb4f088634c2f226f51b98
ryoheisato <thesugar87@gmail.com>
2020-03-09 21:14:56 +0900

    fixed bus

--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,3 @@
-import Error from 'next/error'
-import fetch from 'isomorphic-unfetch'
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
